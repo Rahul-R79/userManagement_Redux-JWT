e676558202843e9b8fd0b6e72d023c0f397d8195
Rahul-R79 <rahul.devworks@gmail.com>
2025-07-21 11:46:47 +0530

    jwt implemented

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
 import User from "../model/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const signin = async(req, res, next)=>{
     const {userName, email, password} = req.body;
@@ -11,3 +13,23 @@ export const signin = async(req, res, next)=>{
     }
 }
 
+export const login = async(req, res, next)=>{
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return res.status(400).json({message: 'Invalid email or password'});
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) return res.status(400).json({message: 'Invalid email or password'});
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        res.cookie('access-token', token, 
+            {httpOnly: true, 
+            secure: true,
+            sameSite: 'Strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json(rest);
+    }catch(err){
+        next(err);
+    }
+}
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import {signin} from '../controllers/authController.js';
+import {login, signin} from '../controllers/authController.js';
 import { validateSignin } from '../middleware/validators.js';
 
 router.post('/signin', validateSignin, signin);
+router.post('/login', login);
 
 export default router;
